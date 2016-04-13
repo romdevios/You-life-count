6d352df244ac2453213ca0af36234131ff6808e8
yrister <yrister@mail.ru>
2016-04-13 13:39:54 +0300

    Initial Commit

---
+++ You life count/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  You life count
+//
+//  Created by Roman Filippov on 13/04/16.
+//  Copyright © 2016 Roman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ You life count/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  You life count
+//
+//  Created by Roman Filippov on 13/04/16.
+//  Copyright © 2016 Roman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
